da7afae286c49734783c428433e2a3457109637f
Declan Possnett <declanpossnett@gmail.com>
2025-11-24 09:23:07 +0000

    project setup

--- quasar.config.ts
+++ quasar.config.ts
@@ -1,7 +1,8 @@
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file
 
-import { defineConfig } from '#q-app/wrappers';
+import { defineConfig } from '#q-app/wrappers'
+import { fileURLToPath } from 'node:url'
 
 export default defineConfig((/* ctx */) => {
   return {
@@ -32,6 +33,10 @@ export default defineConfig((/* ctx */) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
     build: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+
       target: {
         browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
         node: 'node20',
@@ -68,7 +73,8 @@ export default defineConfig((/* ctx */) => {
           {
             vueTsc: true,
             eslint: {
-              lintCommand: 'eslint -c ./eslint.config.js "./src*/**/*.{ts,js,mjs,cjs,vue}"',
+              lintCommand:
+                'eslint -c ./eslint.config.js "./src*/**/*.{ts,js,mjs,cjs,vue}"',
               useFlatConfig: true,
             },
           },
@@ -213,5 +219,5 @@ export default defineConfig((/* ctx */) => {
        */
       extraScripts: [],
     },
-  };
-});
+  }
+})
