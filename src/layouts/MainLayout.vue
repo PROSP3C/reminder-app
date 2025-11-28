24afb22f6be801f1750e535a17a75816d429856b
Declan Possnett <declanpossnett@gmail.com>
2025-11-28 13:05:06 +0000

    update primary color and title

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -16,7 +16,7 @@
           <!-- <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
           </q-avatar> -->
-          DP Reminder App
+          Reminders
         </q-toolbar-title>
 
         <q-btn
